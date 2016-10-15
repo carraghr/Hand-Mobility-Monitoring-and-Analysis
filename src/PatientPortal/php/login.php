9fb0002f1ad81ca20800859f31303a785f43f23f
Richard Carragher <carraghr@gmail.com>
2016-10-15 12:20:29 +0100

    Restructure of files

--- src/PatientPortal/php/login.php
+++ src/PatientPortal/php/login.php
@@ -1,54 +1,39 @@
 <?php
 
-$PageTitle = 'Login to System';
-include ('../html/header.html');
+    $PageTitle = 'Login to System';
+    include ('../html/header.html');
 
 
-/*Check for form submission*/
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    /*Check for form submission*/
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
-    //Helper functions
-    require ('loginFunctions.php');
-    require ('../../databaseConnection.php');
+        //Helper functions
+        require ('loginFunctions.php');
+        require ('../../databaseConnection.php');
 
-    //check the login
-    list($check, $data) = validateLoginDetails($databaseConnection,$_POST['username'],$_POST['password']);
+        //check the login
+        list($check, $data) = validateLoginDetails($databaseConnection,$_POST['username'],$_POST['password']);
 
-    //check if login details are right
-    if ($check){
-        //Set session data
-        session_start();
+        //check if login details are right
+        if ($check){
+            //Set session data
+            session_start();
 
-        $_SESSION['user_id'] = $data['PatientID'];
+            $_SESSION['user_id'] = $data['PatientID'];
 
-        redirectUser('loggedIn.php');
+            redirectUser('loggedIn.php');
 
-    }else{
-        $errors = $data;
-        foreach($errors as $error){
-            echo $error;
+        }else{
+            $errors = $data;
+            foreach($errors as $error){
+                echo $error;
+            }
         }
+
+        mysqli_close($databaseConnection);
+
     }
 
-    mysqli_close($databaseConnection);
-
-}
-?>
-
-    <div class = "content">
-        <div class = "loginBox">
-            <div class = "LogIn_message">
-                <p>Login into Patient Portal</p>
-            </div>
-            <form id = "patientLogin"  action = "login.php" accept-charset="utf-8" method="POST">
-                <input type="text" name="username"  <?php if (isset($_POST['username'])) { echo 'value = ' . $_POST['username'];} else ' placeholder="Username" '; ?> required autofocus/>
-                <br/>
-                <input type="password" name="password" placeholder="Password" required/>
-                <br/>
-                <input type="submit" name = "login" value ="Sign in" >
-            </form>
-        </div>
-    </div>
-<?php
-include ('../html/footer.html');
+    include ('../html/content.html');
+    include ('../html/footer.html');
 ?>
