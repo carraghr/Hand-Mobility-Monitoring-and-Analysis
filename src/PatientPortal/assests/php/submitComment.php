669739069917227dcef0ce1901a2de03eea0636b
Richard Carragher <carraghr@gmail.com>
2016-11-26 20:01:48 +0000

    Backend method for adding comment to database from patient

---
+++ src/PatientPortal/assests/php/submitComment.php
@@ -0,0 +1,25 @@
+<?php
+    header("Content-type: application/x-www-form-urlencoded");
+
+    $SID = $_POST['PHPSESSID'];
+
+
+    session_id($SID);
+    session_start();
+
+    //require('./loginFunctions.php');
+    if(!isset($_SESSION['user_id'])){
+        //need to echo an error
+        echo "failed";
+    }else {
+
+        require('../../../databaseConnection.php');
+
+        $PID = $_SESSION['user_id'];
+        $comment = $_POST['Comment'];
+        $DateTime = date("Y-m-d H:i:s");
+        $query = "insert into feedback values ('$PID','$DateTime','$comment')";
+        $queryResult = @mysqli_query($databaseConnection, $query) OR trigger_error($databaseConnection->error."[ $query]");
+
+        echo $queryResult;
+    }
