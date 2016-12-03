b8f8742e7d764af957237c8153f996426405ae9f
Richard Carragher <carraghr@gmail.com>
2016-12-03 20:51:12 +0000

    angularjs app for care provider portal for exercise view

--- src/CareProviderPortal/app/app.js
+++ src/CareProviderPortal/app/app.js
@@ -1,4 +1,25 @@
-var portal = angular.module('portal',['ngCookies','ngRoute','ngAria']);
+var portal = angular.module('portal',['ngCookies','ngRoute','ngAria','ui.bootstrap','ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+
+portal.config(function ($routeProvider){
+	$routeProvider
+		.when('/',{
+			templateUrl : './app/components/exercises/exercises.html',
+			controller : 'exerciseScheme'
+		})
+		.when('/exercises',{
+			templateUrl : './app/components/exercises/exercises.html',
+			controller : 'exerciseScheme'
+		})
+		.when('/report',{
+			templateUrl : './app/components/exercises/exercises.html',
+			controller : 'exerciseScheme'
+		})
+		.when('/account',{
+			templateUrl : './app/components/exercises/exercises.html',
+			controller : 'exerciseScheme'
+		})
+});
+
 
 portal.controller('patientCtrl',function exercises($scope,$http,$location,$cookies,$httpParamSerializerJQLike){
 
@@ -8,11 +29,14 @@ portal.controller('patientCtrl',function exercises($scope,$http,$location,$cooki
 	var that = this;
 
 	this.getPatientData = function(){
+
+		$cookies.put('PatientID',$scope.patientID);
+
 		request = {
 			method: 'POST',
 			url: location + '/assests/php/patientLookup.php',
 			headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-			data: $httpParamSerializerJQLike({ 'PHPSESSID' : $cookies.get('PHPSESSID'), 'PatientID' : $scope.patientID})
+			data: $httpParamSerializerJQLike({ 'PHPSESSID' : $cookies.get('PHPSESSID'), 'PatientID' : $cookies.get('PatientID')})
 		};
 
 		$http(request).then(function (response){
@@ -23,7 +47,11 @@ portal.controller('patientCtrl',function exercises($scope,$http,$location,$cooki
 	}
 });
 
-portal.controller('exerciseScheme', function exercises($scope,$http,$location,$cookies,$httpParamSerializerJQLike){
+portal.controller('exerciseScheme', function exercises($scope,$http,$location,$cookies,$httpParamSerializerJQLike,$document,$uibModal){
+
+	$scope.templates =
+		[{ name: 'template1.html', url: 'template1.html'},
+			{ name: 'template2.html', url: 'template2.html'}];
 
 	let location = $location.absUrl().substring(0,$location.absUrl().lastIndexOf("CareProviderPortal/")+18);
 
@@ -32,15 +60,107 @@ portal.controller('exerciseScheme', function exercises($scope,$http,$location,$c
 			method: 'POST',
 			url: location + '/assests/php/exerciseTargetsLookup.php',
 			headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-			data: $httpParamSerializerJQLike({ 'PHPSESSID' : $cookies.get('PHPSESSID'), 'PatientID' : PID})
+			data: $httpParamSerializerJQLike({ 'PHPSESSID' : $cookies.get('PHPSESSID'), 'PatientID' : $cookies.get('PatientID')})
 		};
 
 		$http(request).then(function (response){
 			$scope.status = response.status;
 			$scope.patientExericseInfo = response.data;
 		},function(response){});
+		$scope.template = './app/components/exercises/setExercises.html';
 	}
 
+	$scope.getAdditionalExercises = function (){
+		let request = {
+			method: 'POST',
+			url: location + '/assests/php/additionalExercises.php',
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+			data: $httpParamSerializerJQLike({ 'PHPSESSID' : $cookies.get('PHPSESSID'), 'PatientID' : $cookies.get('PatientID')})
+		};
 
+		$http(request).then(function (response){
+			$scope.status = response.status;
+			$scope.additionalExericseInfo = response.data;
+		},function(response){});
 
+		$scope.template = './app/components/exercises/addExercises.html';
+	}
+
+	var getExerciseInformation = function (name){
+		let exercises = $scope.additionalExericseInfo['exercises'];
+		for(let i=0;i<exercises.length;i++){
+			if(exercises[i].Name === name ){
+				return exercises[i];
+			}
+		}
+	}
+
+	var remove = function (name){
+		let exercises = $scope.additionalExericseInfo['exercises'];
+		for(let i=0;i<exercises.length;i++){
+			if(exercises[i].Name === name ){
+				exercises.splice(i, 1);
+				$scope.additionalExericseInfo['exercises'] = exercises;
+			}
+		}
+	}
+
+	$scope.selectedExerciseToAdd = function(name,parentSelector){
+		$scope.selectedExercise =  getExerciseInformation(name);
+		var parentElem = parentSelector ? angular.element($document[0].querySelector(parentSelector)) : undefined;
+		var modalInstance = $uibModal.open({
+				animation: true,
+				ariaLabelledBy: 'modal-title',
+				ariaDescribedBy: 'modal-body',
+				templateUrl: './app/components/exercises/exerciseAdditionForm.html',
+				controller: 'ExerciseFormController',
+				controllerAs: '$form',
+				size: 'lg',
+				appendTo: parentElem,
+				resolve: {
+					exercise: $scope.selectedExercise
+				}
+			}
+		);
+
+		modalInstance.result.then(function (addedExerciseName) {
+			//use Added Exercise Name to seen notification of exercise been added to top of the page.
+			remove(addedExerciseName)
+		}, function(){
+			//$log.info('Modal dismissed at: ' + new Date());
+		});
+	}
+
+	$scope.isActive = function (viewLocation) {
+		console.log($location.path());
+		return viewLocation === $location.path() || ($location.path() === "/" && viewLocation==="/exercises");
+	};
+
+});
+
+portal.controller('ExerciseFormController', function ($uibModalInstance, $http, $httpParamSerializerJQLike, $cookies, exercise) {
+
+	var $form = this;
+	$form.exercise = exercise;
+
+	$form.ok = function () {
+		let formData = $('form[name ="ExerciseAdditionForm"]').serializeArray();
+		let request = {
+			method: 'POST',
+			url: location + '/assests/php/addExercise.php',
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+			data: $httpParamSerializerJQLike({ 'PHPSESSID' : $cookies.get('PHPSESSID'), 'PatientID' : $cookies.get('PatientID'),'form':formData})
+		};
+
+		$http(request).then(function (response){
+			let remove = response.data;
+			console.log(response.data);
+			$uibModalInstance.close(remove);
+		},function(response){});
+
+	};
+
+	$form.cancel = function () {
+		$uibModalInstance.dismiss('cancel');
+	};
 });
